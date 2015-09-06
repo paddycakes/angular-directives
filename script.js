d972d831886c76ea0757a10e094efb3618227869
Patrick Gallagher <paddy.gallagher@gmail.com>
2015-09-06 13:43:17 +0100

    Using templates to add both scope based variables and normal HTML

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ angular.module("app", []);
 
 angular.module('app').controller('mainCtrl', function($scope) {
   $scope.user = {
-    name: 'Luke Skywalker'
+    name: 'Luke Skywalker',
+    address: {
+      street: 'PO Box 123',
+      city: 'Mos Eisley',
+      planet: 'Tatooine'
+    }
   }
 });
 
 angular.module('app').directive('userInfoCard', function() {
     return {
-      template: "Name: {{ user.name }}",
+      template: "Name: {{ user.name }}<br/>Address:<br/>{{ user.address.street }}<br/>{{ user.address.city}}<br/>{{ user.address.planet }}",
       restrict: "E"
     }
 });
