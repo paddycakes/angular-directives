23145c31ee5126ea76cb6a748caf751f3ac2992b
Patrick Gallagher <paddy.gallagher@gmail.com>
2015-09-06 13:51:42 +0100

    Using other directives inside directive template value

--- script.js
+++ script.js
@@ -7,13 +7,18 @@ angular.module('app').controller('mainCtrl', function($scope) {
       street: 'PO Box 123',
       city: 'Mos Eisley',
       planet: 'Tatooine'
-    }
+    },
+    friends: [
+        'Han',
+        'Leia',
+        'Chewbacca'
+    ]
   }
 });
 
 angular.module('app').directive('userInfoCard', function() {
     return {
-      template: "Name: {{ user.name }}<br/>Address:<br/>{{ user.address.street }}<br/>{{ user.address.city}}<br/>{{ user.address.planet }}",
+      template: "Name: {{ user.name }}<br/><br/><div ng-show='!!user.address'>Address:<br/>{{ user.address.street }}<br/>{{ user.address.city}}<br/>{{ user.address.planet }}</div><br/><div>Friends: <div ng-repeat='friend in user.friends'>{{ friend }}</div></div>",
       restrict: "E"
     }
 });
